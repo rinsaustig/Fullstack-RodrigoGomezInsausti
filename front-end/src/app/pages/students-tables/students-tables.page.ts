97d802ed14cd6a93bf549dd05a3fbcb00d9e8399
Rodrigo Gómez Insausti <rinsausti@eon6.com.ar>
2022-04-24 19:01:40 -0300

    Update students-tables.page.ts
    limpiando código

--- front-end/src/app/pages/students-tables/students-tables.page.ts
+++ front-end/src/app/pages/students-tables/students-tables.page.ts
@@ -1,6 +1,6 @@
 import { environment } from './../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
